ca63c402c841d273c815a11c5cec6e825f2b03c8
noob-hu-yaar <48630662+noob-hu-yaar@users.noreply.github.com>
2020-05-10 11:13:50 +0530

    Add files via upload

---
+++ code(Book Shop).cpp
@@ -0,0 +1,44 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n,x;
+    cin>>n>>x;
+
+    vector<int> page(n),price(n);
+    int a,b;
+
+    for(int&i : price)
+    cin>>i;
+
+    for(int&i : page)
+    cin>>i;
+
+    vector<vector<int>> dp(n+1,vector<int>(x+1,0));
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<=x;j++)
+        {
+            //option1 = book number i-1 is not included, hence no pages added.
+            //option2 = book number i-1 is included, hence pages are added.
+            
+            dp[i][j] = dp[i-1][j];
+            if(j >= price[i-1])
+            {
+                dp[i][j] = max (dp[i][j],dp[i-1][j-price[i-1]] + page[i-1]); //max of both the boxes included in dp[i][j]. 
+            }
+        }
+    }
+
+    cout<< dp[n][x] <<endl;
+
+    return 0;
+}
